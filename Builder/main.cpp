d8ab68464dfc8ab2a884ff6ca01ce0f1cde7a10e
manid2 <maniyoung.d2@gmail.com>
2018-01-22 19:20:33 +0530

    Builder pattern[1] for xml dom creation.

--- Builder/main.cpp
+++ Builder/main.cpp
@@ -1,40 +1,81 @@
 #include <iostream>
 
+#include "inc/cxmldom.h"
+
+// Forward declarations
+class Burger;
+class BurgerBuilder;
+
 class Burger {
 public:
-    class BurgerBuilder;
-    void showFlavors() {
-        std::cout << size_;
-        if (cheese_) std::cout << "-cheese";
-        if (peperoni_) std::cout << "-peperoni";
-        if (lettuce_) std::cout << "-lettuce";
-        if (tomato_) std::cout << "-tomato";
-        std::cout << std::endl;
-    }
-private:
-    Burger(int size): size_(size) {}
+   Burger(int size) :
+         m_size(size) {
+   }
+   void showFlavors() {
+      std::cout << m_size;
+      if(m_cheese)
+         std::cout << "-cheese";
+      if(m_peperoni)
+         std::cout << "-peperoni";
+      if(m_lettuce)
+         std::cout << "-lettuce";
+      if(m_tomato)
+         std::cout << "-tomato";
+      std::cout << std::endl;
+   }
 
-    int size_ = 7;
-    bool cheese_ = false;
-    bool peperoni_ = false;
-    bool lettuce_ = false;
-    bool tomato_ = false;
-};
-
-class Burger::BurgerBuilder {
 public:
-    BurgerBuilder(int size) { burger_ = new Burger(size); }
-    BurgerBuilder& AddCheese() { burger_->cheese_ = true; return *this; }
-    BurgerBuilder& AddPepperoni() { burger_->peperoni_ = true; return *this; }
-    BurgerBuilder& AddLettuce() { burger_->lettuce_ = true; return *this; }
-    BurgerBuilder& AddTomato() { burger_->tomato_ = true; return *this; }
-    Burger* Build() { return burger_; }
-private:
-    Burger* burger_;
+   int m_size = 7;
+   bool m_cheese = false;
+   bool m_peperoni = false;
+   bool m_lettuce = false;
+   bool m_tomato = false;
 };
 
-int main()
-{
-    Burger* burger = Burger::BurgerBuilder(14).AddPepperoni().AddLettuce().AddTomato().Build();
-    burger->showFlavors();
+class BurgerBuilder {
+public:
+   BurgerBuilder(int size) {
+      m_burger = new Burger(size);
+   }
+   BurgerBuilder& AddCheese() {
+      m_burger->m_cheese = true;
+      return *this;
+   }
+   BurgerBuilder& AddPepperoni() {
+      m_burger->m_peperoni = true;
+      return *this;
+   }
+   BurgerBuilder& AddLettuce() {
+      m_burger->m_lettuce = true;
+      return *this;
+   }
+   BurgerBuilder& AddTomato() {
+      m_burger->m_tomato = true;
+      return *this;
+   }
+   Burger* Build() {
+      return m_burger;
+   }
+private:
+   Burger* m_burger;
+};
+
+
+using namespace mani_dp;
+
+int main() {
+   Burger* burger = BurgerBuilder(14).AddPepperoni().AddLettuce()./*.AddTomato().*/Build();
+   burger->showFlavors();
+
+
+   ManiXmlDOMVerionNode *verNode = new ManiXmlDOMVerionNode(1, 2);
+   ManiXmlDOMInfoNode   *infoNode = new ManiXmlDOMInfoNode("Mani_info");
+   ManiXmlDOMRootNode *rootNode = new ManiXmlDOMRootNode(verNode, infoNode);
+
+   ManiXmlDOM *xmlDom = new ManiXmlDOM(rootNode);
+
+   XMLString dom(xmlDom->build());
+   std::cout << "\nMani DOM sample\n"
+             << dom
+             << "\n";
 }
